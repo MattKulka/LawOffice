7245eff8072aed0b1eceb544b1e57f5b1f49ee7d
MattKulka <MatthewKulka@outlook.com>
2022-09-16 00:56:03 -0400

    Class Clean up

--- src/main/java/src/main/java/dao/Consultation.java
+++ src/main/java/src/main/java/dao/Consultation.java
@@ -1,4 +1,12 @@
 package src.main.java.dao;
 
 public class Consultation {
+
+    public static String[] ConsultFee;
+
+    public static Integer hours;
+
+    public static void main(String[] args) {
+
+    }
 }
--- src/main/java/src/main/java/service/Main.java
+++ src/main/java/src/main/java/service/Main.java
@@ -1,4 +1,8 @@
 package src.main.java.service;
 
 public class Main {
+
+    public static void main(String[] args) {
+
+    }
 }
