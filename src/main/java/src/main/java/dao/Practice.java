b0e32ad54f2ad9b518e9a770b1c526b840062e69
MattKulka <MatthewKulka@outlook.com>
2022-09-15 20:43:17 -0400

    Practice Information Filled out

---
+++ src/main/java/src/main/java/dao/Practice.java
@@ -0,0 +1,39 @@
+package src.main.java.dao;
+
+import java.util.Arrays;
+
+public class Practice {
+
+    public static void main(String[] args) {
+
+        String[] FLCrimes = new String[5];
+        String[] TVCrimes = new String[5];
+        String[] FedCrimes = new String[5];
+        String[] Category = new String[3];
+
+        Category[0] = "Family Law";
+        Category[1] = "Traffic Violations";
+        Category[2] = "Federal Crimes";
+
+        FLCrimes[0] = "Divorce";
+        FLCrimes[0] = "Adoption";
+        FLCrimes[0] = "Guardianship";
+        FLCrimes[0] = "Domestic Abuse";
+        FLCrimes[0] = "Child Abuse";
+
+        TVCrimes[0] = "Speeding Ticket";
+        TVCrimes[0] = "Parking Violation";
+        TVCrimes[0] = "Reckless Operation";
+        TVCrimes[0] = "DUI";
+        TVCrimes[0] = "Street Racing";
+
+        FedCrimes[0] = "Money Laundering";
+        FedCrimes[0] = "Kidnapping";
+        FedCrimes[0] = "Identity Theft";
+        FedCrimes[0] = "Robbery";
+        FedCrimes[0] = "Terrorism";
+
+
+
+    }
+}
