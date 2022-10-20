31be628b6b74e484459947a488471371eb47d2dc
MattKulka <MatthewKulka@outlook.com>
2022-10-20 15:26:53 -0400

    SAX and json Implementation

---
+++ src/main/java/src/main/java/utility/Resources.java
@@ -0,0 +1,8 @@
+package src.main.java.utility;
+
+public interface Resources {
+    public static final String basePath = "src/main/resources/";
+    public static final String xmlPath = basePath + "xml/";
+    public static final String xsdPath = basePath + "xsd/";
+    public static final String jsonPath = basePath + "json/";
+}
